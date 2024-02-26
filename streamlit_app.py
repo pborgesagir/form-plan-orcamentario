e1af250b805b6808442ba7cd8f629f577c678f0c
pborgesagir <151963659+pborgesagir@users.noreply.github.com>
2024-02-26 15:42:27 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,9 +6,14 @@ from streamlit import text_input
 
 
 
+st.set_page_config(
+    page_title='Formulário para Planejamento Orçamentário',
+    page_icon='📝'
+)
+
 # Display Title and Description
 st.title("Formulário para Planejamento Orçamentário")
-st.page_icon='🏗️'
+
 
 
 # Constants
